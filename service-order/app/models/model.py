eae6e0d2f7224c1bebfbc91b6c01ee4e7d514036
johan sebastian <baloncestonba222@gmail.com>
2025-05-25 09:25:09 +0200

    feat: create the model for service order and create the factory for service order

---
+++ service-order/app/models/model.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Enum
+
+from app import db
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    customer_name: Mapped[str] = mapped_column(db.String(100), nullable=False)
+    id_product: Mapped[int] = mapped_column(db.Integer, nullable=False) 
+    status: Mapped[str] = mapped_column(Enum('pending','paid'), default='pending', nullable=False)
+    total_amount: Mapped[float] = mapped_column(db.Float, nullable=False)
